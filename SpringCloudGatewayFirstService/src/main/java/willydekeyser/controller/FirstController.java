1dcf1910c8456e4f81c275961292b437ed38e66d
willydekeyser <ma074172@skynet.be>
2023-04-11 08:54:50 +0200

    Update Spring Cloud Gateway

--- SpringCloudGatewayFirstService/src/main/java/willydekeyser/controller/FirstController.java
+++ SpringCloudGatewayFirstService/src/main/java/willydekeyser/controller/FirstController.java
@@ -1,6 +1,7 @@
 package willydekeyser.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
 
 	@GetMapping("/message")
-	public String test() {
-		return "Hello Spring Boot Tutorial Called in First Service";
+	public String test(@RequestHeader("first-request") String header) {
+		return "Hello Spring Boot Tutorial Called in First Service " + header;
 	}
 }
